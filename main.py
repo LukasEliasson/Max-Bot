b9540b9c1439f4fdd213dc7767ea98840c92590c
alvar <alvarnivert@gmail.com>
2025-05-10 22:36:46 +0200

    Jag fixa näringsämenna så att det fukar tror jag

--- main.py
+++ main.py
@@ -9,58 +9,22 @@ max_text: str = BeautifulSoup(net.text, 'html.parser').text
 lines = max_text.splitlines()
 filtered_lines = []
 per_portion = {}
-banned_words = ["Integritetsmeddelande",]
 for line in lines:
     line = line.strip()
-    if line != '':
-        filtered_lines.append(line)
+    filtered_lines.append(line)
 
-filtered_lines = filtered_lines[73:]
-
-value_counter = 0
-latest_key = ""
-table_headers = [
-    'Supreme Green',
-    'Burgare',
-    'Kyckling, fisk & sallad',
-    'Tillbehör & Dip',
-    'Desserter & Lyxshake',
-    'Dryck & Kaffe',
-]
-between_tables = False
-total_count = 0
+combination = []
 for line in filtered_lines:
-    if total_count >= 500:
-        break
+    if line == "":
+        if len(combination) > 5:
+            if combination[0] not in per_portion.keys():
+                per_portion[combination[0]] = combination[1:]
+        combination = []
+    else:
+        combination.append(line)
 
-    if not line in per_portion.keys():
-        if line in table_headers:
-            between_tables = True
-            value_counter = 0
-            print(f'{line}: Set between tables to True')
-        elif between_tables:
-            if line == '(i kg)':
-                between_tables = False
-                print(f'{line}: Set between tables to False')
-            else:
-                print(f'{line}: Skipped')
-        elif value_counter == 0:
-            per_portion[line] = []
-            latest_key = line
-            value_counter += 1
-            print(f'{line}: New key')
-        elif len(line) > 13:
-            value_counter = 0
-            print(f'{line}: Set value counter to 0 due to long line.')
-        elif value_counter < 10:
-            per_portion[latest_key].append(line)
-            value_counter += 1
-            print(f'{line}: Set value ({value_counter})')
-        else:
-            value_counter = 0
-            print(f'{line}: Set value counter to 0.')
 
-        total_count += 1
+
 
 max_menu = res.json()
 
